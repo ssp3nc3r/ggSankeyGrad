854359ab099702b17b0914503e0a5704f9e7f165
ssp3nc3r <ssp3nc3r@users.noreply.github.com>
2020-03-29 20:52:00 -0400

    Initial commit

---
+++ R/ggSankeyGrad.R
@@ -0,0 +1,126 @@
+# gg_sankey_grad
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+
+
+ggSankeyGrad <- function(c1, c2,
+                           col1 = "gray", col2 = "gray",
+                           values,
+                           padding = 2,
+                           alpha = 0.4,
+                           label = FALSE,
+                           lwidth= 0.5) {
+
+  stopifnot(requireNamespace("dplyr"))
+  stopifnot(requireNamespace("ggplot2"))
+  df <- data.frame(c1, c2, values)
+
+  # calculate beginning bottom and top of flow ribbons
+  d1 <- df %>%
+    group_by(c1, c2) %>%
+    summarise(lengths = sum(values)) %>%
+    ungroup() %>%
+    mutate(b1 = lag(cumsum(lengths +
+                             ifelse(row_number() < n() &
+                                      c1 == lead(c1),
+                                    0, padding )))) %>%
+    mutate(b1 = ifelse(is.na(b1), 0, b1)) %>%
+    mutate(t1 = b1 + lengths) %>%
+    select(-lengths)
+
+  # calculate ending bottom and top of flow ribbons
+  d2 <- df %>%
+    group_by(c2, c1) %>%
+    summarise(lengths = sum(values)) %>%
+    ungroup() %>%
+    mutate(b2 = lag(cumsum(lengths +
+                             ifelse(row_number() < n() &
+                                      c2 == lead(c2),
+                                    0, padding )))) %>%
+    mutate(b2 = ifelse(is.na(b2), 0, b2)) %>%
+    mutate(t2 = b2 + lengths) %>%
+    select(-lengths)
+
+  # combine calculations with beginning and ending colors for each flow
+  d <- left_join(d1, d2, by = c("c1", "c2"))
+  d$col1 <- col1
+  d$col2 <- col2
+
+  # create a factor for each flow
+  d <- d %>% mutate(cat = as.integer(as.factor(paste0(c1, c2))))
+
+  # x value for each vertical line
+  x <- seq(0, 1, length = 1000)
+
+  # calculate bottom and top y value for each line
+  bez <- data.frame()
+  for(i in seq(nrow(d))) {
+    bot <- with(d[i,],
+                matrix(c(0,b1, 1,b1, 3,b2, 4,b2),
+                       nrow = 4, ncol = 2, byrow = TRUE))
+    top <- with(d[i,],
+                matrix(c(0,t1, 1,t1, 3,t2, 4,t2),
+                       nrow = 4, ncol = 2, byrow = TRUE))
+    col <- with(d[i,],
+                colorRampPalette(c(as.character(col1),
+                                   as.character(col2)))( length(x) ))
+    bez <- bind_rows(bez,
+                     data.frame(cat = i,
+                                x = x,
+                                col = col,
+                                bez_b = bezier::bezier(t = x, p = bot)[,2],
+                                bez_t = bezier::bezier(t = x, p = top)[,2],
+                                stringsAsFactors = FALSE) )
+  }
+
+  # create base plot with lines
+  pl <- ggplot() +
+    scale_x_continuous(limits = c(-0.2, 1.2)) +
+    geom_segment(data = bez,
+                 aes(x = x, xend = x,
+                     y = bez_b, yend = bez_t),
+                 color = bez$col,
+                 size = lwidth,
+                 alpha = alpha)
+
+  # add labels for beginning (left) and ending (right) categories
+  if(label == T) {
+
+    loc <- d %>%
+      group_by(c1) %>%
+      slice(n()) %>%
+      ungroup() %>%
+      mutate(y = ifelse(row_number() == 1, t1/2 ,
+                        (t1 + lag(t1) + padding)/2) )
+
+    for(i in seq(nrow(loc))) {
+      pl <- pl + annotate('text', x = -0.01, y = loc$y[i],
+                          label = as.character(loc$c1[i]),
+                          hjust = 1, size = 10/.pt)
+    }
+
+    loc <- d %>%
+      group_by(c2) %>%
+      slice(n()) %>%
+      ungroup() %>%
+      mutate(y = ifelse(row_number() == 1, t2/2 ,
+                        (t2 + lag(t2) + padding)/2) )
+
+
+    for(i in seq(nrow(loc))) {
+      pl <- pl + annotate('text', x = 1.01, y = loc$y[i],
+                          label = as.character(loc$c2[i]),
+                          hjust = 0, size = 10/.pt)
+    }
+
+
+  }
+
+  # remove all theme elements
+  pl <- pl + theme_void()
+
+  # return ggplot object
+  return(pl)
+}
